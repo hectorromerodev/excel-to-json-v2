8823f6e733f223215be28cf5dced6be2184b7a5c
Hector Romero <hectorromerodev@gmail.com>
2021-02-09 21:56:50 -0800

    ADD Shared module

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BrowseFileButtonComponent } from './components/browse-file-button/browse-file-button.component';
+import { AngularMaterialModule } from '../angular-material.module';
+
+
+
+@NgModule({
+  declarations: [BrowseFileButtonComponent],
+  imports: [
+    CommonModule,
+    AngularMaterialModule
+  ]
+})
+export class SharedModule { }
